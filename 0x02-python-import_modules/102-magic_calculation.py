472b390b11845e823cff443fb51d447e537bbf48
geraldinnebohr <geraldinnebohr@gmail.com>
2019-09-13 04:35:37 +0000

    [FEATURE] Python function that does the same as bytecode

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/bin/usr/python3
+def magic_calculation(a, b):
+from calculator_1 import add, sub
+if a < b:
+    return c = add(a, b)
+for i in range(:90):
+    c = add(c, i)
+    return c
+sub(a, b)
+    return 0
