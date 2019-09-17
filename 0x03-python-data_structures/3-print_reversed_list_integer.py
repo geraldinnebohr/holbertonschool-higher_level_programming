ce3860626473bb3892608f87d6afc895a23051d4
geraldinnebohr <geraldinnebohr@gmail.com>
2019-09-17 18:43:45 +0000

    [FIX] condition if list is empty

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-        if my_list == []:
+        if my_list is None:
                 return
         for i in reversed(range(len(my_list))):
-                print("{}".format(my_list[i]))
+                print("{:d}".format(my_list[i]))
