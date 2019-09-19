2c9e00b3e9083d8d72a76bbb471a432b1d3cfc99
geraldinnebohr <geraldinnebohr@gmail.com>
2019-09-19 23:49:27 +0000

    [FIX] dont count the length of keys instead the one with maximum value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,5 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    max_val = max(key for key, value in a_dictionary.items())
+    max_val = max(a_dictionary, key=a_dictionary.get)
     return max_val
