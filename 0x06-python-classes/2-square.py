490895f1ca8e93a8c951e11d6a02542145494801
geraldinnebohr <geraldinnebohr@gmail.com>
2019-10-01 20:02:27 +0000

    [FEATURE] class square that defines a square by a private instance attribute

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Square:
+    """This is a Square class empty"""
+
+    def __init__(self, size=0):
+        """This is the __init__ method"""
+
+        if type(size) is not int:
+            raise ValueError('size must be >= 0')
+        elif size < 0:
+            raise TypeError('size must be an integer')
+        else:
+            self._Square__size = size
