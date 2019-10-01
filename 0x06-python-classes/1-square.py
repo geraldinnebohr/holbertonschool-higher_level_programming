3eeb4886033dc4e81e97f10a3ccc825e2a7b8d8e
geraldinnebohr <geraldinnebohr@gmail.com>
2019-10-01 19:48:05 +0000

    [FEATURE] class Square that defines a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class Square:
+    """This is a Square class empty"""
+
+    def __init__(self, size):
+        """This is a private instance attribute"""
+
+        self._Square__size = size
