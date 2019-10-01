cf6f62c981424e8483bdfb8ffa7999617101659b
geraldinnebohr <geraldinnebohr@gmail.com>
2019-10-01 21:53:49 +0000

    [FIX] conditions with correct error message

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,8 +6,8 @@ class Square:
         """This is the __init__ method"""
 
         if type(size) is not int:
-            raise ValueError('size must be >= 0')
-        elif size < 0:
             raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
         else:
             self._Square__size = size
