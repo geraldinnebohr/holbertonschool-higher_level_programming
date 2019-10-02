93428e99a2922b78c304cbcbd23841a60c343a90
geraldinnebohr <geraldinnebohr@gmail.com>
2019-10-02 04:01:09 +0000

    [FIX] added print empty

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 class Square:
-    """This is a Square class empty"""
+
+    """This is a Square class"""
 
     def __init__(self, size=0, position=(0, 0)):
-
-        self.size = size
+        """This is a public instance method"""
         self.position = position
+        self.size = size
 
     def area(self):
         """This is a public instance method"""
@@ -19,11 +20,10 @@ class Square:
     @size.setter
     def size(self, value):
         """This is a public instance method"""
-
         if type(value) is not int:
-            raise TypeError('size must be an integer')
+            raise TypeError("size must be an integer")
         elif value < 0:
-            raise ValueError('size must be >= 0')
+            raise ValueError("size must be >= 0")
         else:
             self.__size = value
 
@@ -32,26 +32,23 @@ class Square:
         """This is a public instance method"""
         return self.__position
 
-    @size.setter
+    @position.setter
     def position(self, value):
         """This is a public instance method"""
-
-        if \
-           type(value) is not tuple or len(value) is not 2 or \
-           type(value[0]) is not int or type(value[1]) is not int \
-           or value[0] < 0 or value[1] < 0:
-            raise TypeError('position must be a tuple of 2 positive integers')
+        if (type(value) is not tuple or
+               len(value) is not 2 or type(value[0]) is not int or
+                type(value[1]) is not int or value[0] < 0 or value[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
 
     def my_print(self):
-        """This is a public instance method"""
-
-        if self.__size:
-            print("\n" * self.__position[1], end="")
-            for i in range(self.__size):
-                print(" " * self.__position[0], end="")
-                print("#" * self.__size, end="")
+        """Print a square method"""
+        if self.__size is 0:
             print("")
         else:
-            print("")
+            if self.__position[1] is not 0:
+                print("\n" * self.__position[1], end='')
+            for i in range(self.__size):
+                print(" " * self.__position[0], end='')
+                print("#" * self.__size)
