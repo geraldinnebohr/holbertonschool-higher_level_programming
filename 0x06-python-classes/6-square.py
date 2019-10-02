0769d63a28867addf27ae8e7591d58a254a8bf4a
geraldinnebohr <geraldinnebohr@gmail.com>
2019-10-02 03:37:16 +0000

    [FIX] changed condition in my_print method

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -38,7 +38,7 @@ class Square:
 
         if \
            type(value) is not tuple or len(value) is not 2 or \
-           type(value[0]) is not int  or type(value[1]) is not int \
+           type(value[0]) is not int or type(value[1]) is not int \
            or value[0] < 0 or value[1] < 0:
             raise TypeError('position must be a tuple of 2 positive integers')
         else:
@@ -47,7 +47,7 @@ class Square:
     def my_print(self):
         """This is a public instance method"""
 
-        if self.__size is not 0:
+        if self.__size:
             print("\n" * self.__position[1], end="")
             for i in range(self.__size):
                 print(" " * self.__position[0], end="")
