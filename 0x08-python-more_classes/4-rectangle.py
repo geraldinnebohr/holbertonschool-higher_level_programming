0152a4186ce6e23963515b491fe62e2385f050db
geraldinnebohr <geraldinnebohr@gmail.com>
2019-10-08 02:26:20 +0000

    [FIX] private instances

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -10,8 +10,8 @@ class Rectangle:
     """
 
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     def __str__(self):
         if self.__width is 0 or self.__height is 0:
