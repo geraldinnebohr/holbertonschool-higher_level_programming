4b3b73b65c97cdd41386136f4ba2ae39a28e22ac
geraldinnebohr <geraldinnebohr@gmail.com>
2019-10-23 02:57:18 +0000

    [FIX] serializing and deserializing csv files

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,7 @@ fref
 """
 import os.path
 import json
+import csv
 
 
 class Base:
@@ -70,3 +71,40 @@ class Base:
                 return new_list
         else:
             return new_list
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """
+        write csv file
+        """
+        if cls.__name__ is "Rectangle":
+            headers = ["id", "width", "height", "x", "y"]
+        elif cls.__name__ is "Square":
+            headers = ["id", "size", "x", "y"]
+        with open(cls.__name__ + ".csv", "w", encoding="utf-8") as f:
+            csv_writer = csv.DictWriter(f, fieldnames=(headers))
+            csv_writer.writeheader()
+            empty_list = []
+            for i in list_objs:
+                my_dict = i.to_dictionary()
+                empty_list.append(my_dict)
+                csv_writer.writerow(my_dict)
+            return empty_list
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """
+        from csv files
+        """
+        lt = []
+        if os.path.isfile(cls.__name__ + '.csv'):
+            with open(cls.__name__ + '.csv',
+                      encoding='utf-8') as csv_file:
+                csv_write = csv.DictReader(csv_file)
+                for dic in csv_write:
+                    for key, value in dic.items():
+                        dic[key] = int(value)
+                    lt.append(cls.create(**dic))
+                return lt
+        else:
+            return lt
