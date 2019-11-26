62fb6eb6efea7ffb251661bf133179bc709348eb
geraldinnebohr <geraldinnebohr@gmail.com>
2019-11-26 18:02:47 +0000

    [FEATURE] scripts

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- script that creates a database and a table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(
-       id INT PRIMARY KEY,
+       id INT PRIMARY KEY AUTO_INCREMENT,
        name  VARCHAR(256) NOT NULL
 );
--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,6 +2,6 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
        id INT PRIMARY KEY,
-       state_id INT FOREIGN KEY(id.states)
+       state_id INT NOT NULL FOREIGN KEY(states_id) REFERENCES states(id)
        name  VARCHAR(256) NOT NULL
 );
