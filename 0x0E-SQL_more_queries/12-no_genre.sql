80a08e5d3f612b20a655bcda0801bf32f770eddc
geraldinnebohr <geraldinnebohr@gmail.com>
2019-11-26 22:22:31 +0000

    [FEATURE] scripts

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- script that lists all shows contained in the database that
+-- have at least one genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+     ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_show_genres.genre_id is NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
