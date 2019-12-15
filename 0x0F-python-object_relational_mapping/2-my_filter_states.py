8a76357ca1b65d37cf84e310429665014b1b95ad
geraldinnebohr <geraldinnebohr@gmail.com>
2019-12-15 05:08:53 +0000

    [FIX] loop condition

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -14,10 +14,11 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=mysql_user,
                          passwd=mysql_passwd, db=mysql_db)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id".
+    cur.execute("SELECT * FROM states ORDER BY id".
                 format(mysql_name))
     query_rows = cur.fetchall()
     for row in query_rows:
-        print(row)
+        if row[1] is mysql_name:
+            print(row)
     cur.close()
     db.close()
