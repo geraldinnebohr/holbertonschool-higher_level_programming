94614879643868ac3306ffbc59db2d20c4cc6de3
geraldinnebohr <geraldinnebohr@gmail.com>
2019-12-15 05:05:24 +0000

    [FIX] loop condition

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,9 +12,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=mysql_user,
                          passwd=mysql_passwd, db=mysql_db)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name REGEXP '^[N]' ORDER BY id")
+    cur.execute("SELECT * FROM states ORDER BY id")
     query_rows = cur.fetchall()
     for row in query_rows:
-        print(row)
+        if row[1][0] is 'N':
+            print(row)
     cur.close()
     db.close()
