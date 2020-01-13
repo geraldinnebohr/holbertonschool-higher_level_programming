619058f108cf5db5911323e5bd80025576db20ca
geraldinnebohr <geraldinnebohr@gmail.com>
2020-01-13 21:04:53 +0000

    [FEATURE] - What's my status?

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Python script that fetches an URL"""
+from urllib import request
+
+
+with request.urlopen('https://intranet.hbtn.io/status') as r:
+    url = r.read()
+    t = type(url)
+    u = url.decode('utf-8')
+    print("Body Response:")
+    print("\t- type:", t)
+    print("\t- content:", url)
+    print("\t- utf8 content:", u)
