368d333b6caa216c00b9a4f2faf4f053d72baebd
geraldinnebohr <geraldinnebohr@gmail.com>
2020-01-14 02:13:08 +0000

    [FIX] except

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
     r = requests.post('http://0.0.0.0:5000/search_user', q)
     try:
         json = r.json()
+        if len(json) == 2:
+            print('[{}] {}'.format(json.get('id'), json.get('name')))
+        else:
+            print('No result')
     except:
         print('Not a valid JSON')
-    if json.get('id') and json.get('name') is not None:
-        print('[{}] {}'.format(json.get('id'), json.get('name')))
-    else:
-        print('No result')
