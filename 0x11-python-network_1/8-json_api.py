f6f4def5555ff45d8c78ff3b2ae508238adfe5f0
geraldinnebohr <geraldinnebohr@gmail.com>
2020-01-14 01:46:20 +0000

    [FEATURE] Python script that takes in a letter and sends a POST request to an API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Python script that sends an email as a parameter in a URL"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        q = ''
+    else:
+        q = {'q': sys.argv[1]}
+    r = requests.post('http://0.0.0.0:5000/search_user', q)
+    try:
+        json = r.json()
+    except:
+        print('Not a valid JSON')
+    try:
+        print('[{}] {}'.format(json['id'], json['name']))
+    except:
+        print('No result')
