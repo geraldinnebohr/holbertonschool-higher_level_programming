09ddd66c718e84bd298ce9ccd971a648847482f3
Geraldinne Bohr <geraldinnebohr@gmail.com>
2020-02-04 18:21:39 +0000

    [FIX] cast to int

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,9 @@ if (!process.argv[2] || !process.argv[3]) {
   console.log(0);
 } else {
   const args = process.argv.slice(2);
-  args.sort();
+  const intArgs = args.map(function (x) {
+    return parseInt(x, 10);
+  });
+  console.log(intArgs.sort());
   console.log(args[args.length - 2]);
 }
