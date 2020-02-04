44ec73e5b82a151e1c6a7b8e23ce8266df9449b9
Geraldinne Bohr <geraldinnebohr@gmail.com>
2020-02-04 18:57:11 +0000

    [FIX] code

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,9 +3,6 @@ if (!process.argv[2] || !process.argv[3]) {
   console.log(0);
 } else {
   const args = process.argv.slice(2);
-  const intArgs = args.map(function (x) {
-    return parseInt(x, 10);
-  });
-  intArgs.sort();
-  console.log(args[args.length - 2]);
+  const intArgs = args.sort();
+  console.log(intArgs[intArgs.length - 2]);
 }
