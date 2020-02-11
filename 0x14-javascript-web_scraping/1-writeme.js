19d470e528a547edd9001a8f8995e41758cafecd
Geraldinne Bohr <geraldinnebohr@gmail.com>
2020-02-11 18:03:17 +0000

    [FIX] file as parameter

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-const data = process.argv[2];
-fs.writeFile('my_file.txt', data, (err) => {
+const data = process.argv[3];
+fs.writeFile(process.argv[2], data, (err) => {
   if (err) console.log(err);
 });
